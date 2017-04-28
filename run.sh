75bfcbae4174c575884c7c7c1197b73dfd28be49
bjohnson <ben@gophronesis.com>
2017-04-27 21:13:18 -0400

    fixing whiten

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@
 
 find ./oxford/data/ -type f | ./threaded-featurize.py --outpath ./oxford.h5
 find ./paris/data/ -type f | ./threaded-featurize.py --outpath ./paris.h5
+
+python whiten.py --index-features ./oxford.h5 --whiten-features ./paris.h5 --outpath ./oxford-white
+python whiten.py --index-features ./paris.h5 --whiten-features ./oxford.h5 --outpath ./paris-white
--- whiten.py
+++ whiten.py
@@ -2,11 +2,17 @@
 
 """
     whiten.py
+    
+    Learns whitening featurization from one dataset, applies to another
 """
 
+import sys
 import h5py
+import argparse
 import numpy as np
+
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
 
 def h5py2iterator(x, k=''):
     if isinstance(x, h5py._hl.group.Group):
@@ -16,24 +22,32 @@ def h5py2iterator(x, k=''):
     else:
         yield k,x.value
 
-# --
-# Params
-
-d = 128
-whiten_features = './paris.h5'
-index_features = './oxford.h5'
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index-features', type=str, default='./oxford.h5')
+    parser.add_argument('--whiten-features', type=str, default='./paris.h5')
+    parser.add_argument('--dim', type=int, default=128)
+    parser.add_argument('--outpath', type=str, required=True)
+    return parser.parse_args()
 
 # --
 # Run
 
-# Learn whitening params from one dataset
-wdata = np.vstack([v.sum(axis=(1, 2)) for k,v in h5py2iterator(h5py.File(whiten_features))])
-nwdata = normalize(wdata)
-white_pca = PCA(n_components=d, whiten=True).fit(nwdata)
+if __name__ == "__main__":
+    args = parse_args()
+    
+    # Learn whitening params from one dataset
+    print >> sys.stderr, 'learning transform from %s' % args.whiten_features
+    wdata = np.vstack([v.sum(axis=(1, 2)) for k,v in h5py2iterator(h5py.File(args.whiten_features))])
+    nwdata = normalize(wdata)
+    white_pca = PCA(n_components=args.dim, whiten=True, svd_solver='full').fit(nwdata)
 
-# Applying whitening to second dataset
-data = np.vstack([v.sum(axis=(1, 2)) for k,v in h5py2iterator(h5py.File(index_features))])
-ndata = normalize(data)
-white_ndata = white_pca.transform(ndata)
-nwhite_ndata = normalize(white_ndata)
-
+    # Applying whitening to second dataset
+    print >> sys.stderr, 'applying transform to %s' % args.index_features
+    data = np.vstack([v.sum(axis=(1, 2)) for k,v in h5py2iterator(h5py.File(args.index_features))])
+    ndata = normalize(data)
+    white_ndata = white_pca.transform(ndata)
+    nwhite_ndata = normalize(white_ndata)
+    
+    print >> sys.stderr, 'saving to %s' % args.outpath
+    np.save(args.outpath, nwhite_ndata)
